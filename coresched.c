a758ac93ef4241f580fe68825f4f8fbaa6cb3700
Thijs Raymakers <thijs@raymakers.nl>
2024-03-12 12:34:17 +0100

    Simplify command line options

--- coresched.c
+++ coresched.c
@@ -2,39 +2,32 @@
 // Licensed under the EUPL v1.2
 
 #include <argp.h>
-#include <asm-generic/errno-base.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <linux/prctl.h>
 #include <string.h>
+#include <stdbool.h>
+
+static char args_doc[] = "get -p [PID]\n"
+			 "create -p [PID]\n"
+			 "copy -p [PID] -d [PID]";
 
 static char doc[] = "Manage core scheduling cookies for tasks";
 
-#define GETTYPE_OPT 1
-#define NEWTYPE_OPT 2
-#define FROMTYPE_OPT 3
-#define TOTYPE_OPT 4
-
-static struct argp_option options[] = {
-	{ 0, 0, 0, 0, "Getting core scheduling cookies", 1 },
-	{ "get", 'g', "PID", 0, "get the core scheduling cookie of pid", 1 },
-	{ "gettype", GETTYPE_OPT, "TYPE", 0,
-	  "type of the pid to get the cookie from. Can be one of the following: pid, tgid or pgid. Defaults to pgid.",
+static struct argp_option options[7] = {
+	{ 0, 0, 0, 0, "Source PID", 1 },
+	{ "pid", 'p', "PID", 0, "source pid of the core scheduling cookie", 1 },
+	{ "type", 't', "TYPE", 0,
+	  "type of the pid. Can be one of the following: pid, tgid or pgid. Defaults to pgid.",
 	  1 },
-	{ 0, 0, 0, 0, "Creating new core scheduling cookies", 2 },
-	{ "new", 'n', "PID", 0, "create a new unique cookie for pid", 2 },
-	{ "newtype", NEWTYPE_OPT, "TYPE", 0,
-	  "type of the pid for the new cookie. Can be one of the following: pid, tgid or pgid. Defaults to pgid.",
+	{ 0, 0, 0, 0, "Destination PID for copying the core scheduling cookie",
 	  2 },
-	{ 0, 0, 0, 0, "Copying core scheduling cookies", 3 },
-	{ "from", 'f', "PID", 0, "pid to pull the core schedling cookie from",
-	  3 },
-	{ "fromtype", FROMTYPE_OPT, "TYPE", 0,
-	  "type of the 'from' pid. Can be one of the following: pid, tgid or pgid. Defaults to pgid.",
-	  3 },
-	{ "to", 't', "PID", 0, "pid to push the core schedling cookie to", 3 },
-	{ "totype", TOTYPE_OPT, "TYPE", 0,
-	  "type of the 'to' pid. Can be one of the following: pid, tgid or pgid. Defaults to pgid.",
-	  3 },
+	{ "destpid", 'd', "PID", 0, "pid to copy the core scheduling cookie to",
+	  2 },
+	{ "desttype", 'e', "TYPE", 0,
+	  "type of the destpid. Can be one of the following: pid, tgid or pgid. Defaults to pgid.",
+	  2 },
+	{ 0 }
 };
 
 typedef enum {
@@ -43,18 +36,43 @@ typedef enum {
 	SCHED_CORE_SCOPE_PGID = PR_SCHED_CORE_SCOPE_PROCESS_GROUP,
 } core_sched_type_t;
 
+typedef enum {
+	SCHED_CORE_CMD_GET,
+	SCHED_CORE_CMD_CREATE,
+	SCHED_CORE_CMD_COPY,
+} core_sched_cmd_t;
+
 struct args {
-	pid_t get_pid;
-	pid_t new_pid;
 	pid_t from_pid;
 	pid_t to_pid;
-	core_sched_type_t get_type;
-	core_sched_type_t new_type;
 	core_sched_type_t from_type;
 	core_sched_type_t to_type;
+	core_sched_cmd_t cmd;
 };
 
-pid_t parse_pid(char *str, struct argp_state *state)
+bool verify_arguments(struct argp_state *state, struct args *args,
+		      char **error_msg)
+{
+	if (args->from_pid != 0) {
+		if (args->cmd == SCHED_CORE_CMD_COPY && args->to_pid == 0) {
+			char *msg =
+				"Copying a core scheduling cookie requires a destination PID\0";
+			const size_t msg_len = strlen(msg);
+			*error_msg = malloc(msg_len);
+			memcpy(*error_msg, msg, msg_len);
+			return false;
+		}
+		return true;
+	}
+	char *msg =
+		"Retrieving a core scheduling cookie requires a source PID\0";
+	const size_t msg_len = strlen(msg);
+	*error_msg = malloc(msg_len);
+	memcpy(*error_msg, msg, msg_len);
+	return false;
+}
+
+pid_t parse_pid(struct argp_state *state, char *str)
 {
 	const int base = 10;
 	char *tailptr = NULL;
@@ -69,7 +87,7 @@ pid_t parse_pid(char *str, struct argp_state *state)
 	}
 }
 
-core_sched_type_t parse_core_sched_type(char *str, struct argp_state *state)
+core_sched_type_t parse_core_sched_type(struct argp_state *state, char *str)
 {
 	if (!strncmp(str, "pid\0", 4)) {
 		return SCHED_CORE_SCOPE_PID;
@@ -85,37 +103,49 @@ core_sched_type_t parse_core_sched_type(char *str, struct argp_state *state)
 	__builtin_unreachable();
 }
 
+core_sched_cmd_t parse_cmd(struct argp_state *state, char *arg)
+{
+	if (!strncmp(arg, "get\0", 4)) {
+		return SCHED_CORE_CMD_GET;
+	} else if (!strncmp(arg, "create\0", 7)) {
+		return SCHED_CORE_CMD_CREATE;
+	} else if (!strncmp(arg, "copy\0", 5)) {
+		return SCHED_CORE_CMD_COPY;
+	} else {
+		argp_error(state, "Unknown command '%s'", arg);
+		__builtin_unreachable();
+	}
+}
+
 error_t parse_opt(int key, char *arg, struct argp_state *state)
 {
 	struct args *arguments = state->input;
 
 	switch (key) {
-	case 'g':
-		arguments->get_pid = parse_pid(arg, state);
+	case ARGP_KEY_ARG:
+		arguments->cmd = parse_cmd(state, arg);
 		break;
-	case GETTYPE_OPT:
-		arguments->get_type = parse_core_sched_type(arg, state);
-		break;
-	case 'n':
-		arguments->new_pid = parse_pid(arg, state);
-		break;
-	case NEWTYPE_OPT:
-		arguments->new_type = parse_core_sched_type(arg, state);
-		break;
-	case 'f':
-		arguments->from_pid = parse_pid(arg, state);
-		break;
-	case FROMTYPE_OPT:
-		arguments->from_type = parse_core_sched_type(arg, state);
+	case 'p':
+		arguments->from_pid = parse_pid(state, arg);
 		break;
 	case 't':
-		arguments->to_pid = parse_pid(arg, state);
+		arguments->from_type = parse_core_sched_type(state, arg);
 		break;
-	case TOTYPE_OPT:
-		arguments->to_type = parse_core_sched_type(arg, state);
+	case 'd':
+		arguments->to_pid = parse_pid(state, arg);
 		break;
-	case ARGP_KEY_NO_ARGS:
-		argp_usage(state);
+	case 'e':
+		arguments->to_type = parse_core_sched_type(state, arg);
+		break;
+	case ARGP_KEY_SUCCESS:
+		if (state->argc <= 1) {
+			argp_usage(state);
+		}
+		char *error_msg = NULL;
+		if (!verify_arguments(state, arguments, &error_msg)) {
+			assert(error_msg != NULL);
+			argp_error(state, "%s", error_msg);
+		}
 		break;
 	default:
 		return ARGP_ERR_UNKNOWN;
@@ -126,7 +156,10 @@ error_t parse_opt(int key, char *arg, struct argp_state *state)
 int main(int argc, char *argv[argc])
 {
 	struct args arguments = { 0 };
-	struct argp argp = { options, parse_opt, 0, doc, 0, 0, 0 };
+	arguments.from_type = SCHED_CORE_SCOPE_PGID;
+	arguments.to_type = SCHED_CORE_SCOPE_PGID;
+
+	struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 	exit(0);
